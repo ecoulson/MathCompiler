fdf7aa9a0b46667a14a8b0a7856c80b110d3fae0
ecoulson <ivansonofcoul@gmail.com>
2017-12-21 13:47:39 -0800

    added lexer

--- LispCompiler/Parser.cs
+++ LispCompiler/Parser.cs
@@ -4,20 +4,22 @@ namespace LispCompiler
     public class Parser
     {
         private TokenStream tokenStream;
-        private ParseNode head;
 
         public Parser(TokenStream tokenStream)
         {
             this.tokenStream = tokenStream;
-            this.head = null;
         }
 
-        public ParseNode Parse() {
+        public ParseNode Parse()
+        {
             return ParseExpression();
         }
 
-        private ParseNode ParseExpression() {
+        private ParseNode ParseExpression()
+        {
+            return null;
         }
+    }
 }
 
 // goal
